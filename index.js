bb80e4ab4f4471b83edb5c3d1a57d4450978d2e4
Gabriel Weich <gabrielweich@hotmail.com>
2018-02-20 09:14:21 -0300

    src folder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const CoinList = require('./coinlist');
-const Coin = require('./coin');
+const CoinList = require('./src/coinlist');
+const Coin = require('./src/coin');
 const config = {
   interval: process.env.INTERVAL ? process.env.INTERVAL.toString() : '1h',
   window: process.env.WINDOW ? parseInt(process.env.WINDOW) : 10,
@@ -28,8 +28,8 @@ const addCoins = () => {
   this.coinList.add(new Coin('IOTA', 5, 4, 0));
   this.coinList.add(new Coin('NEO', 6, 3, 2));
   this.coinList.add(new Coin('QTUM', 5, 3, 2));
-  this.coinList.add(new Coin('LSK', 6, 9, 2));
-  this.coinList.add(new Coin('XVG', 5, 5, 0));
+  this.coinList.add(new Coin('LSK', 6, 6, 2));
+  this.coinList.add(new Coin('XVG', 5, 6, 0));
   this.coinList.add(new Coin('ZEC', 5, 3, 3));
   this.coinList.add(new Coin('EOS', 6, 4, 0));
   this.coinList.add(new Coin('NANO', 5, 4, 2));
